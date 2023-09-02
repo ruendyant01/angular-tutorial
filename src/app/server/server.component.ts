4d710ce7aae790fb055418041b859a1e6b4b7738
Ruendyant <ruendyant.mayraclle@gmail.com>
2023-09-02 19:57:15 +0700

    done

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+@Component({
+  selector: "server-comp",
+  templateUrl: "./server.component.html"
+})
+export class ServerComponent {
+  mainTitle: string = "Server"
+}
